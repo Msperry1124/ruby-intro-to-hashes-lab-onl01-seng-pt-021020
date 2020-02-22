cecb70558275cd8e3fc6131a5fc25d803b801729
Melissa Sperry <sperry.mel@gmail.com>
2020-02-22 06:45:16 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -29,7 +29,7 @@ def monopoly_with_third_tier
 	monopoly = {
 
 			:railroads=>
-	end			{
+		{
 			:pieces=>4,
 			:names=>
 			{
@@ -52,5 +52,5 @@ def monopoly_with_third_tier
 end
 
 def monopoly_with_fourth_tier
-	
+
 end
